eedfe36e9ddf8592cc6a457fb722dcfb9aa8ee29
Mike Moran <lazysurefix@gmail.com>
2018-04-30 23:45:09 +0100

    very basic layout exercising bulma styles

--- view/src/App.js
+++ view/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <section className="section">
+          <div className="container">
+            <h1 className="title">
+              Hello World
+            </h1>
+            <p className="subtitle">
+              My first website with <strong>Bulma</strong>!
+            </p>
+          </div>
+        </section>
+        <footer className="footer">
+          <div className="container">
+            <div className="content has-text-centered">
+              <p>
+                <strong>Baton</strong> by <a href="http:/houseofmoran.com">Mike Moran</a>.
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
