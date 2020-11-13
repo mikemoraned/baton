d7efbe8c3a030d370ba864507cac06e9d5192426
Mike Moran <mike@houseofmoran.com>
2020-11-13 20:56:39 +0000

    wasm is loading

---
+++ view/src/components/QrCode.js
@@ -0,0 +1,25 @@
+import React from "react";
+export function lazyLoader() {
+    console.time("loading baton libs");
+    return Promise.all([
+        import("@mike_moran/baton-lib"),
+        import("@mike_moran/baton-lib/baton_lib_bg"),
+    ]).then(([baton_lib, baton_lib_bg]) => {
+        console.timeEnd("loading baton libs");
+        return bindQrCode({
+            baton_lib,
+            baton_lib_bg,
+        });
+    });
+}
+
+function bindQrCode({ baton_lib, baton_lib_bg }) {
+    const { BatonLib } = baton_lib;
+    const { memory } = baton_lib_bg;
+
+    function QrCode() {
+        return <div>QrCode</div>
+    }
+
+    return { default: QrCode };
+}
--- view/src/pages/Home.js
+++ view/src/pages/Home.js
@@ -1,3 +1,13 @@
+import React, { Suspense } from "react";
+import { lazyLoader } from "../components/QrCode";
+
+const QrCode = React.lazy(lazyLoader);
+
 export function Home() {
-  return <h2>Home</h2>;
+  return (<div>
+      <h2>Home</h2>
+      <Suspense fallback={<div>loading</div>}>
+        <QrCode />
+      </Suspense>
+ </div>);
 }
