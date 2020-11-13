7a3d509042548abe45e5428375851e78f5d60e0e
Mike Moran <mike@houseofmoran.com>
2020-11-13 23:08:30 +0000

    use fixed `0.2.3` version

--- view/src/components/QrCode.js
+++ view/src/components/QrCode.js
@@ -1,21 +1,19 @@
 import React from "react";
+
 export function lazyLoader() {
     console.time("loading baton libs");
     return Promise.all([
         import("@mike_moran/baton-lib"),
-        import("@mike_moran/baton-lib/baton_lib_bg"),
-    ]).then(([baton_lib, baton_lib_bg]) => {
+    ]).then(([baton_lib]) => {
         console.timeEnd("loading baton libs");
         return bindQrCode({
-            baton_lib,
-            baton_lib_bg,
+            baton_lib
         });
     });
 }
 
-function bindQrCode({ baton_lib, baton_lib_bg }) {
+function bindQrCode({ baton_lib }) {
     const { QrCodeGenerator } = baton_lib;
-    const { memory } = baton_lib_bg;
 
     function QrCode() {
         const generator = QrCodeGenerator.new(10, 10);
