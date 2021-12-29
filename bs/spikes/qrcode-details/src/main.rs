aee5b0c770214ee7d25fea8edb6c66f68db07c45
Mike Moran <mike@houseofmoran.com>
2021-12-30 00:24:13 +0100

    output each element

--- bs/spikes/qrcode-details/src/main.rs
+++ bs/spikes/qrcode-details/src/main.rs
@@ -1,3 +1,19 @@
+use qrcode_generator::QrCodeEcc;
+
 fn main() {
-    println!("Hello, world!");
+    let message = "Hello world";
+
+    let result: Vec<Vec<bool>> = qrcode_generator::to_matrix(message, QrCodeEcc::High).unwrap();
+
+    for row in result.iter() {
+        let rendered_row : String = row.iter().map( |item| {
+            if *item {
+                '#'
+            }
+            else {
+                ' '
+            }
+        }).collect();
+        println!("{}", rendered_row);
+    }
 }
