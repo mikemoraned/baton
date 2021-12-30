ee0c4535aa3b1ebbca5907ce77751f4a09366327
Mike Moran <mike@houseofmoran.com>
2021-12-30 01:15:22 +0100

    convert vecs to div's and span's

--- bs/spikes/qrcode-in-html/src/main.rs
+++ bs/spikes/qrcode-in-html/src/main.rs
@@ -1,58 +1,45 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 markup::define! {
-    Home<'a>(title: &'a str) {
+    Home<'a>(filled: &'a Vec<Vec<bool>>) {
         @markup::doctype()
         html {
             head {
-                title { @title }
-                style {
-                    "body { background: #fafbfc; }"
-                    "#main { padding: 2rem; }"
-                }
+                // style {
+                //     "body { background: #fafbfc; }"
+                //     "#main { padding: 2rem; }"
+                // }
             }
             body {
-                @Header { title }
-                #main {
-                    p {
-                        "This domain is for use in illustrative examples in documents. You may \
-                        use this domain in literature without prior coordination or asking for \
-                        permission."
-                    }
-                    p {
-                        a[href = "https://www.iana.org/domains/example"] {
-                            "More information..."
+                div {
+                    @for row in filled.iter() {
+                        div {
+                            @for entry in row.iter() {
+                                @if *entry {
+                                    span.filled { "#" }
+                                }
+                                else {
+                                    span.empty { " "}
+                                }
+                            }
                         }
                     }
                 }
-                @Footer { year: 2020 }
             }
-        }
-    }
-
-    Header<'a>(title: &'a str) {
-        header {
-            h1 { @title }
-        }
-    }
-
-    Footer(year: u32) {
-        footer {
-            "(c) " @year
         }
     }
 }
 
 async fn greet() -> impl Responder {
+    let filled = vec![vec![true]];
     HttpResponse::Ok()
         .content_type("text/html")
         .body(format!("{}",
             Home {
-                title: "Example Domain"
+                filled: &filled
             }
         ))
 }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
